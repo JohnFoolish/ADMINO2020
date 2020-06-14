1adbb060c31e9afff4bfa9b67ed2f433fd501066
tnbowes <tnbowes@gmail.com>
2020-06-14 17:12:12 -0400

    Automated Commit

--- Code.ts
+++ Code.ts
@@ -5,6 +5,8 @@ const ssBattalion = ss.getSheetByName('Battalion Structure');
 const ssOptions = ss.getSheetByName('Options');
 const ssPending = ss.getSheetByName('Pending Paperwork');
 
+const form = FormApp.openByUrl('https://docs.google.com/forms/d/1l6lZZhsOWb5rcyTFDxyiFJln0tFBuVIiFRGK_hjnZ84/edit');
+
 /*
   const data = ssData.getRange(1,1,ssData.getLastRow(), ssData.getLastColumn()).getValues();   
   ssData.getRange(1,1,ssData.getLastRow(), ssData.getLastColumn()).setValues(data);
@@ -18,10 +20,48 @@ function myOnSubmit() {
 		// Manipulate data
 
 		//Write to data sheet
-		ssData.getRange(ssData.getLastRow(), 1, ssData.getLastRow() + 1, data[0].length).setValues(data);
+		ssData.getRange(ssData.getLastRow() + 1, 1, 1, data[0].length).setValues(data);
 	}
 }
 
 function myOnEdit() {
 	// Not sure what is going to end up going here
+}
+
+//Make the sheet pretty function--once a week?
+
+function updateFormGroups() {
+	const item = form.addListItem();
+	item.setTitle('Receiever Name/Group');
+	const groups = getGroups();
+	const groupList = [];
+	for (const groupData of groups) {
+		groupList.push(item.createChoice(groupData));
+	}
+	item.setChoices(groupList);
+	item.isRequired();
+	item.setHelpText('The group or MIDN you want to assign the paperwork to');
+	Logger.log(groupList);
+}
+
+function getGroups(): string[] {
+	const groupData = ssBattalion.getRange(1, 1, ssBattalion.getLastRow(), ssBattalion.getLastColumn()).getValues();
+	const out = [];
+
+	for (let i = 3; i < groupData[0].length; i++) {
+		const group = groupData[0][i];
+		if (group !== '') {
+			out.push(group);
+		}
+	}
+
+	for (let i = 1; i < groupData.length; i++) {
+		const person = groupData[i][0] + groupData[i][1];
+		if (person !== '') {
+			out.push(person);
+		}
+	}
+
+	Logger.log(out);
+	return out;
 }
