cb65296bfaabd14323a1eb272772769539b423a1
tnbowes <tnbowes@gmail.com>
2020-06-14 17:22:50 -0400

    Automated Commit

--- Code.ts
+++ Code.ts
@@ -33,6 +33,7 @@ function myOnEdit() {
 function updateFormGroups() {
 	const FormItem = form.getItems();
 	const item = FormItem[1].asListItem();
+	item.setChoices([]);
 	item.setTitle('Receiever Name/Group');
 	const groups = getGroups();
 	const groupList = [];
