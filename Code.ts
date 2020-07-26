b271b1eedd50aa65d744d1257b4c5ed0fb8c831b
tnbowes <tnbowes@gmail.com>
2020-07-26 17:00:57 -0400

    Automated Commit

--- Code.ts
+++ Code.ts
@@ -480,7 +480,6 @@ function updateSheet(sheetID, name) {
 	var merits = 0;
 	var negCounsel = 0;
 	const pending = ssPending.getRange(1, 1, ssPending.getLastRow(), ssPending.getLastColumn()).getValues();
-	var data = [['1']];
 	for (var i = 1; i < pending.length; i++) {
 		if (pending[i][3] === name) {
 			Logger.log(pending[i]);
@@ -491,10 +490,9 @@ function updateSheet(sheetID, name) {
 			} else if (pending[i][4] === 'Merit') {
 				merits++;
 			}
-			data = pending[i];
 			//ssData.getRange(ssData.getLastRow() + 1, 1, outData.length, outData[0].length).setValues(outData);
 			// Exception: The parameters (number,number,number,null) don't match the method signature for SpreadsheetApp.Range.setValues.
-			userPaperwork.getRange(userPaperwork.getLastRow() + 1, 1, 1, data[0].length).setValues(data);
+			userPaperwork.getRange(userPaperwork.getLastRow() + 1, 1, 1, pending[i].length).setValues([pending[i]]);
 		}
 	}
 	const helpData = [];
