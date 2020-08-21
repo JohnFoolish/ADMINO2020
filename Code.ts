4377d8f171064d9aaf39b0c152d917c86ef0e1b4
tnbowes <tnbowes@gmail.com>
2020-08-20 21:55:20 -0400

    Automated Commit

--- Code.ts
+++ Code.ts
@@ -1070,7 +1070,7 @@ function processFromAssignemntForm() {
 		Logger.log(
 			`Submit data: ${JSON.stringify(submitData)} ////// RawGridData: ${JSON.stringify(keyValuePairsRawGridCheckbox)}`
 		);
-		myOnAssignmentSubmit(submitData, keyValuePairsRawGridCheckbox);
+		//myOnAssignmentSubmit(submitData, keyValuePairsRawGridCheckbox);
 	});
 	//form.deleteAllResponses();
 
